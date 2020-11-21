ac7bd4524aef716b5266655a9ff41da3ecda9d43
Varsharani Ambi <68917117+varsha-ambi@users.noreply.github.com>
2020-11-21 14:11:34 +0530

    s

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,8 @@
 import React from "react";
-//import "../styles/App.css";
-
 export default function Welcome(props) {
   return (
     <>
-      <h1>Hey{props.name}!</h1>
+      <h1>Hey {props.name}!</h1>
       <h2>Welcome to Newton School</h2>
     </>
   );
